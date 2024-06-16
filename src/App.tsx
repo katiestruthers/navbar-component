1656d0ddadccc2052aabef623bd557f772229f3b
Katie Struthers <99215338+katiestruthers@users.noreply.github.com>
2024-06-16 15:11:52 -0500

    initial set-up

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { NavBar } from './components/NavBar';
+import { Credits } from './components/Credits';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Credits />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+export const NavBar = () => {
+  return (
+    <div> 
+      This is the nav bar!
+    </div>
+  );
+};
