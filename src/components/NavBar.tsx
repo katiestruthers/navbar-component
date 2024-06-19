41c8b0935259507b8f88732639123cf8c45735db
Katie Struthers <99215338+katiestruthers@users.noreply.github.com>
2024-06-19 04:48:59 -0500

    setup styled component slideout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,20 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/anchor-has-content */
+import { useState } from "react";
 import logo from "../img/abstractly.svg";
+import { SlideoutMenu } from "./SlideoutMenu";
 
 type NavProps = {
   titles: string[]
 }
 
 export const NavBar = (props: NavProps) => {
+  const [activeSlideout, setActiveSlideout] = useState(false);
+
   return (
     <header className="flex w-screen justify-between items-center px-28 pt-4"> 
-      <img src={logo} alt="Abstractly logo"></img>
-      <nav className="flex gap-8">
+      <img src={logo} alt="Abstractly logo" className="py-2.5"></img>
+      <nav className="flex gap-8 max-lg:hidden">
         {props.titles.map((title) => {
           return (
             <a href="#">
@@ -19,7 +23,7 @@ export const NavBar = (props: NavProps) => {
           )   
         })}
       </nav>
-      <div className="flex gap-4">
+      <div className="flex gap-4 max-lg:hidden">
         <a href="#">
           <button className="text-neutral-900 text-base font-medium bg-white px-4 py-2.5 rounded shadow" type="button">Learn more</button>
         </a>
@@ -27,6 +31,11 @@ export const NavBar = (props: NavProps) => {
           <button className="text-white text-base font-medium bg-indigo-700 px-4 py-2.5 rounded shadow" type="button">See pricing</button>
         </a>
       </div>
+      
+      <button aria-label="Open mobile menu" className="lg:hidden" onClick={() => { setActiveSlideout(!activeSlideout) }}>
+        <span aria-hidden="true" className="">☰</span>
+      </button>
+      <SlideoutMenu open={activeSlideout} titles={props.titles} />
     </header>
   );
 };
